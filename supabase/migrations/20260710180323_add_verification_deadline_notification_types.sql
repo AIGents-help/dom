4abb879b93ef8326650c723a2918538a3c364596
AIGents.help <contact@aigents.help>
2026-09-19 17:35:22 -0400

    Restore migration 20260710180323 add_verification_deadline_notification_types

---
+++ supabase/migrations/20260710180323_add_verification_deadline_notification_types.sql
@@ -0,0 +1,4 @@
+
+alter type email_notification_type add value 'unverified_pilot_welcome';
+alter type email_notification_type add value 'verification_deadline_reminder';
+alter type email_notification_type add value 'verification_deadline_final';
